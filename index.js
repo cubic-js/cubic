139ae73d51d99f32e8a3d739fd4309dfb9113647
Kaptard <kilian.volb@protonmail.com>
2017-04-25 11:59:16 +0200

    Fixes #3

--- index.js
+++ index.js
@@ -4,21 +4,19 @@ const extend = require('deep-extend')
 const Connection = require('./lib/connection.js')
 const EventEmitter = require('event-emitter-es6')
 
-class Nexus extends EventEmitter {
+class Blitz extends EventEmitter {
 
     /**
      * Merge default options with client options
      */
-    constructor(options, callback) {
+    constructor(options) {
         super()
 
         this.options = extend({
 
             // Resource Config
-            api_url: "https://api.nexus-stats.com/",
-            auth_url: "https://auth.nexus-stats.com/",
-            game: 'warframe',
-            api_version: 'v1',
+            api_url: "https://localhost:3400/",
+            auth_url: "https://localhost:7119/",
 
             // Connection Config
             use_socket: true,
@@ -76,59 +74,6 @@ class Nexus extends EventEmitter {
                 .catch(err => reject(err))
         })
     }
-
-
-    /**
-     * Query method to easily create url from given params
-     */
-    query(verb, query) {
-        let url = this.options.api_url
-
-        // Generate Base URL
-        url += this.options.game + '/'
-        url += this.options.api_version + '/'
-        url += query.resource + '/'
-        url += query.method
-
-        // Dynamically generate rest of URL
-        let prefix = "?"
-        for (var param in query) {
-            if (param !== "resource" && param !== "method") {
-                url += prefix + param + '=' + query[param]
-                prefix = "&"
-            }
-        }
-
-        // Replace spaces with standard encoding
-        url = url.replace(" ", "%20")
-
-        // Send Request
-        return new Promise((resolve, reject) => {
-            this.connection.request(verb, url)
-                .then(res => resolve(res))
-                .catch(err => reject(err))
-        })
-    }
-
-
-    /**
-     * Sample method to get all stats for specific item
-     */
-    getItem(name, options) {
-        let query = {
-            resource: "items/" + name,
-            method: "statistics"
-        }
-
-        // Extend  with options if provided
-        if (options) query = extend(query, options)
-
-        return new Promise((resolve, reject) => {
-            this.query('GET', query)
-                .then(res => resolve(res))
-                .catch(err => reject(err))
-        })
-    }
 }
 
-module.exports = Nexus
+module.exports = Blitz
