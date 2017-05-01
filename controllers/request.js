aa9dd080c1d4194d02563667bbe2ea6c8cd003f8
Kaptard <kilian.volb@protonmail.com>
2017-05-01 22:33:35 +0200

    Fixed cluster request Sync. Added core node timeout check.

--- controllers/request.js
+++ controllers/request.js
@@ -11,13 +11,15 @@ class RequestController {
      * Connect to databases
      */
     constructor(adapter) {
-        this.schema = { uat: 0 }
+        this.schema = {
+            uat: 0
+        }
         this.endpoints = endpoints
         this.adapter = adapter
 
         // Load endpoint config directly on bootup
         this.endpoints.connect()
-        .then(() => this.endpoints.compareSchema(this.adapter))
+            .then(() => this.endpoints.compareSchema(this.adapter))
     }
 
 
@@ -43,12 +45,19 @@ class RequestController {
 
             // Params returned
             else if (request) {
-                this.send(request).then(data => {
-                    resolve({
-                        statusCode: 200,
-                        body: data
+                this.send(request)
+                    .then(data => {
+                        resolve({
+                            statusCode: 200,
+                            body: data
+                        })
                     })
-                })
+                    .catch(err => {
+                        resolve({
+                            statusCode: 503,
+                            body: err
+                        })
+                    })
             }
 
             // No params returned
@@ -63,11 +72,58 @@ class RequestController {
 
 
     /**
-     * Sends request to connected sockets. First response is accepted
-     * Note: figure out way to cancel operations after one node already finished
-     * E.g.: Listen to options.callback with data "cancel" -> stop current progress
+     * Sends request to connected sockets.
      */
     send(options) {
+        return new Promise((resolve, reject) => {
+
+            // Check if nodes available
+            this.check()
+
+                // Send request or respond with busy
+                .then(socket => this.sendRequest(socket, options))
+                .catch(err => reject(err))
+
+                // Respond with data if all went right
+                .then(res => resolve(res))
+        })
+    }
+
+
+    /**
+     * Check if resource nodes are busy
+     */
+    check() {
+        return new Promise((resolve, reject) => {
+
+            // unique callback id
+            let id = process.hrtime().join("").toString()
+
+            // Send check to root nsp
+            this.client.root.emit("check", id)
+            blitz.log.silly("CHK [" + id + "] BROADCAST")
+
+            // Listen to all sockets in root nsp for response
+            Object.keys(this.client.root.sockets).forEach(sid => {
+                let socket = this.client.root.sockets[sid]
+
+                socket.on(id, () => {
+                    socket.removeAllListeners(id)
+                    blitz.log.silly("CHK [" + id + "] ACK IN")
+                    resolve(socket)
+                })
+            })
+
+            // Wait 1 second before rejecting
+            setTimeout(() => reject("All nodes currently busy. Please try again later."), 1000)
+        })
+    }
+
+
+    /**
+     * Send request and receive data
+     */
+    sendRequest(socket, options) {
         return new Promise((resolve, reject) => {
 
             // Generate unique callback for emit & pass to responding node
@@ -75,15 +131,13 @@ class RequestController {
 
             // Send Request to all Core Nodes
             this.client.root.emit("req", options)
+            blitz.log.silly("REQ [" + options.callback + "] OUT")
 
             // Listen to all sockets for response
-            Object.keys(this.client.root.sockets).forEach(id => {
-                let socket = this.client.root.sockets[id]
-
-                socket.on(options.callback, data => {
-                    socket.removeAllListeners(options.callback)
-                    resolve(data)
-                })
+            socket.on(options.callback, data => {
+                socket.removeAllListeners(options.callback)
+                blitz.log.silly("REQ [" + options.callback + "] SUCCESS")
+                resolve(data)
             })
         })
     }
