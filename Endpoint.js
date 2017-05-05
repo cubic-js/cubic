53b9606a1f770488b0aae2ca41a9684969425e66
Nakroma <tarackobarack@gmail.com>
2017-05-05 19:04:58 +0200

    Removed cache method

--- Endpoint.js
+++ Endpoint.js
@@ -40,60 +40,6 @@ class Method{
          */
         this.db = db
     }
-
-    /**
-     * Gets cached values if available
-     * @param {string} collection - Collection to cache
-     * @param {object} query - Current query
-     * @param {object} params - Additional selection parameters
-     * @return {object[]} Cached documents
-     */
-    getCache(collection, query, params = {}) {
-        this.db.collection(collection + '-cache').findOne(query, (err, result) => {
-            // Return object, default empty
-            let docs = []
-
-            // Append objects that fit the params
-            if ("undefined" !== typeof result) {
-                result = result['_cached_documents']
-                for (let i = 0; i < result.length; i++) {
-                    let currentDoc = result[i]
-
-                    // Loop trough params
-                    let paramsCorrect = true
-                    for (let key in params) {
-                        if (params.hasOwnProperty(key)) {
-                            // Does current doc own that key?
-                            if (currentDoc.hasOwnProperty(key)) {
-                                // Is the property selected?
-                                if (currentDoc[key] != params[key]) {
-                                    paramsCorrect = false
-                                    break
-                                }
-                            }
-                        }
-                    }
-
-                    // Append to selected docs
-                    if (paramsCorrect) docs.push(currentDoc)
-                }
-            }
-
-            // Return documents
-            return docs
-        })
-    }
-
-    /**
-     * Caches new values
-     * @param {string} collection - Collection to cache
-     * @param {object} query - Current query
-     * @param {object[]} documents - Documents to cache
-     */
-    setCache(collection, query, documents) {
-        query['_cached_documents'] = documents
-        this.db.collection(collection + '-cache').insertOne(query)
-    }
 }
 
 module.exports = Method
