712598bce66c094250561d884f33f5f28f5020c7
Kaptard <kilian.volb@protonmail.com>
2017-05-27 19:40:24 +0200

    add support for patch queries

--- index.js
+++ index.js
@@ -107,6 +107,16 @@ class Blitz {
         })
     }
 
+    patch(url, body) {
+        let query = {
+            url: url,
+            body: body
+        }
+        return new Promise((resolve, reject) => {
+            this.query("PATCH", query).then((res) => resolve(res))
+        })
+    }
+
     delete(url, body) {
         let query = {
             url: url,
