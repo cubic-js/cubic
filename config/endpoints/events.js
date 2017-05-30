b7d6910870e93c1d8cfc9cb9a1ceaac295b94e71
Kaptard <kilian.volb@protonmail.com>
2017-05-30 12:54:10 +0200

    fixes #27

--- config/endpoints/events.js
+++ config/endpoints/events.js
@@ -46,7 +46,7 @@ module.exports = (sockets, http, cache) => {
         // Listen to Updates from core node and publish to subscribers
         socket.on("publish", update => {
             blitz.log.verbose("API       | > publishing data for " + update.endpoint)
-            sockets.io.to(update.endpoint).emit("new", update)
+            sockets.io.to(update.endpoint).emit(update.endpoint, update.data)
         })
 
         // Listen for Cache updates
