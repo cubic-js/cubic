af3853fe5d7fa658defd8b91187011f6b5bba8c0
Kilian <kilian.volb@protonmail.com>
2017-08-07 07:18:54 +0200

    Support blitz.js overhaul/optimization - Add support for blitz.js sub-nodes inside of a node - Each node now has a blitz.id property - Use ES7 specs where possible - Lots of small fixes/optimizations

--- index.js
+++ index.js
@@ -18,13 +18,22 @@ class Blitz {
      * Set global blitz config system
      */
     constructor(options) {
+        // Merge existing blitz global with current if a new instance is
+        // called inside a worker itself (necessary for hooking further
+        // sub components)
+        if (global.blitz) {
+            blitz = _.merge(this, blitz)
+            blitz.log.class = require("./config/logger.js")
+        }
 
-        // Generate global blitz object
-        global.blitz = this
-        blitz.config = {}
-        blitz.nodes = {}
-        blitz.log = new(require("./config/logger.js"))
-        blitz.log.class = require("./config/logger.js")
+        // No instance was run before
+        else {
+            global.blitz = this
+            blitz.config = {}
+            blitz.nodes = {}
+            blitz.log = new(require("./config/logger.js"))
+            blitz.log.class = require("./config/logger.js")
+        }
 
         let config = {
             local: local,
@@ -39,8 +48,8 @@ class Blitz {
      * Attach module config to global blitz object
      */
     setConfig(id, config) {
-        blitz.config[id] = {}
         let merged = _.merge(config.local, config.provided)
+        blitz.config[id] = {}
 
         // Add each key to global blitz object
         for (var property in merged) {
@@ -53,28 +62,28 @@ class Blitz {
      * Hook functions to be executed before specific node is clustered while making node config available to the Hook
      */
     hook(node, fn) {
-        let nodeid = node.name
+        let id = typeof node === "string" ? node : node.name.toLowerCase()
 
         // Create global node obj if not existing
-        if (!blitz.nodes[nodeid]) {
-            blitz.nodes[nodeid] = {}
+        if (!blitz.nodes[id]) {
+            blitz.nodes[id] = {}
         }
 
         // Create hook stack to be executed before cluster()
-        if (!blitz.nodes[nodeid].hooks) {
-            blitz.nodes[nodeid].hooks = []
+        if (!blitz.nodes[id].hooks) {
+            blitz.nodes[id].hooks = []
         }
 
-        blitz.nodes[nodeid].hooks.push(fn)
+        blitz.nodes[id].hooks.push(fn)
     }
 
 
     /**
      * Execute hooks for specific node
      */
-    runHooks(nodeid) {
-        if (blitz.nodes[nodeid].hooks) {
-            blitz.nodes[nodeid].hooks.forEach(hook => hook())
+    runHooks(id) {
+        if (blitz.nodes[id].hooks) {
+            blitz.nodes[id].hooks.forEach(hook => hook())
         }
     }
 
@@ -83,7 +92,8 @@ class Blitz {
      * Let blitz handle framework modules
      */
     use(node) {
-        let id = node.constructor.name
+        let nid = node.config.provided ? node.config.provided.id : undefined
+        let id = nid ? nid : node.constructor.name.toLowerCase()
 
         // Property already set? Merge them.
         if (blitz.nodes[id]) {
@@ -106,23 +116,28 @@ class Blitz {
      */
     cluster(node, id) {
         let file = node.filename
-        let cores = blitz.config[id].cores
+        let cores = 1 //blitz.config[id].cores
 
         // Fork Workers
         blitz.nodes[id].workers = []
         for (let i = 0; i < cores; i++) {
 
             // Add to node's worker list to be accessible globally
-            blitz.nodes[id].workers.push(fork(file, { env : { isWorker : true } }))
+            blitz.nodes[id].workers.push(fork(file, {
+                env: {
+                    isWorker: true
+                }
+            }))
 
             // Send global blitz to worker
+            blitz.id = id
             blitz.nodes[id].workers[i].send({
                 type: "setGlobal",
                 data: this.serialize(blitz)
             })
 
             // Make Worker methods accessible from global blitz
-            this.setWorkerInterface(node,id)
+            this.exposeMethods(node, id)
 
             // Restart worker on exit
             blitz.nodes[id].workers[i].on("exit", (code, signal) => {
@@ -135,7 +150,7 @@ class Blitz {
     /**
      * Make Worker methods accessible from global blitz
      */
-    setWorkerInterface(node, id) {
+    exposeMethods(node, id) {
         for (let method of Object.getOwnPropertyNames(Object.getPrototypeOf(node))) {
             let _this = this
 
@@ -159,7 +174,7 @@ class Blitz {
      * Serialize global blitz object so it can be sent via stdout to workers
      */
     serialize(obj) {
-        return CircularJSON.stringify(obj, function(key, value) {
+        return CircularJSON.stringify(obj, (key, value) => {
             return (typeof value === 'function') ? value.toString() : value
         })
     }
