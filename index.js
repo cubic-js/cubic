ac379e5786007f4620c85e5ee2c815ea88dd8f8c
Kilian <kilian.volb@protonmail.com>
2017-08-22 02:32:02 +0200

    Update dependencies

--- index.js
+++ index.js
@@ -60,8 +60,8 @@ class View {
      */
     initBlitz() {
         delete process.env.isWorker
-        const Core = require("../blitz-js-core")
-        const API = require("../blitz-js-api")
+        const Core = require("blitz-js-core")
+        const API = require("blitz-js-api")
         const Blitz = require("blitz-js")(blitz.config.local)
 
         // Apply config to nodes and hook them
