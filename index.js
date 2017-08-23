7801717782d625c3eda1195a25fac0dc1afb240f
Kilian <kilian.volb@protonmail.com>
2017-08-23 16:43:59 +0200

    apply standard js

--- index.js
+++ index.js
@@ -2,118 +2,112 @@ const extend = require('deep-extend')
 const Connection = require('./lib/connection.js')
 
 class Blitz {
+  /**
+   * Merge default options with client options
+   */
+  constructor (options) {
+    this.connecting = null
+    this.options = extend({
 
-    /**
-     * Merge default options with client options
-     */
-    constructor(options) {
-        this.connecting = null
-        this.options = extend({
+      // Resource Config
+      api_url: 'http://localhost:3010/',
+      auth_url: 'http://localhost:3030/',
 
-            // Resource Config
-            api_url: "http://localhost:3010/",
-            auth_url: "http://localhost:3030/",
+      // Connection Config
+      use_socket: true,
+      namespace: '/',
 
-            // Connection Config
-            use_socket: true,
-            namespace: '/',
+      // Authorization Config
+      user_key: null,
+      user_secret: null,
+      ignore_limiter: false
+    }, options)
 
-            // Authorization Config
-            user_key: null,
-            user_secret: null,
-            ignore_limiter: false
-        }, options)
+    // Remove "/" from url's
+    let api = this.options.api_url
+    let auth = this.options.auth_url
+    this.options.api_url = api[api.length - 1] === '/' ? api.slice(0, -1) : api
+    this.options.auth_url = auth[auth.length - 1] === '/' ? auth.slice(0, -1) : auth
+    this.connect()
+  }
 
-        // Remove "/" from url's
-        let api = this.options.api_url
-        let auth = this.options.auth_url
-        this.options.api_url = api[api.length - 1] === "/" ? api.slice(0, -1) : api
-        this.options.auth_url = auth[auth.length - 1] === "/" ? auth.slice(0, -1) : auth
-        this.connect()
+  /**
+   * Connect by getting tokens and setting up clients
+   */
+  async connect () {
+    this.connection = new Connection(this.options)
+    this.connecting = this.connection.connect()
+    await this.connecting
+    this.connecting = null
+  }
+
+  /**
+   * Subscribe to certain endpoints
+   */
+  async subscribe (endpoint) {
+    await this.connecting
+    this.emit('subscribe', endpoint)
+  }
+
+  /**
+   * Event listening for socket.io
+   */
+  async on (ev, func) {
+    await this.connecting
+    this.connection.client.on(ev, func)
+  }
+
+  /**
+   * Expose Socket client emit
+   */
+  async emit (ev, data) {
+    await this.connecting
+    this.connection.client.emit(ev, data)
+  }
+
+  /**
+   * RESTful methods for manual interaction
+   */
+  async query (verb, query) {
+    await this.connecting
+    return this.connection.request(verb, query)
+  }
+
+  get (query) {
+    return this.query('GET', query)
+  }
+
+  post (url, body) {
+    let query = {
+      url: url,
+      body: body
     }
+    return this.query('POST', query)
+  }
 
-
-    /**
-     * Connect by getting tokens and setting up clients
-     */
-    async connect() {
-        this.connection = new Connection(this.options)
-        this.connecting = this.connection.connect()
-        await this.connecting
-        this.connecting = null
+  put (url, body) {
+    let query = {
+      url: url,
+      body: body
     }
+    return this.query('PUT', query)
+  }
 
-
-    /**
-     * Subscribe to certain endpoints
-     */
-    async subscribe(endpoint) {
-        await this.connecting
-        this.emit("subscribe", endpoint)
+  patch (url, body) {
+    let query = {
+      url: url,
+      body: body
     }
+    return this.query('PATCH', query)
+  }
 
-
-    /**
-     * Event listening for socket.io
-     */
-    async on(ev, func) {
-        await this.connecting
-        this.connection.client.on(ev, func)
+  delete (url, body) {
+    let query = {
+      url: url,
+      body: body
     }
-
-
-    /**
-     * Expose Socket client emit
-     */
-    async emit(ev, data) {
-        await this.connecting
-        this.connection.client.emit(ev, data)
-    }
-
-
-    /**
-     * RESTful methods for manual interaction
-     */
-    async query(verb, query) {
-        await this.connecting
-        return this.connection.request(verb, query)
-    }
-
-    get(query) {
-        return this.query("GET", query)
-    }
-
-    post(url, body) {
-        let query = {
-            url: url,
-            body: body
-        }
-        return this.query("POST", query)
-    }
-
-    put(url, body) {
-        let query = {
-            url: url,
-            body: body
-        }
-        return this.query("PUT", query)
-    }
-
-    patch(url, body) {
-        let query = {
-            url: url,
-            body: body
-        }
-        return this.query("PATCH", query)
-    }
-
-    delete(url, body) {
-        let query = {
-            url: url,
-            body: body
-        }
-        return this.query("DELETE", query)
-    }
+    return this.query('DELETE', query)
+  }
 }
 
 module.exports = Blitz
