c6257f04ae5ae922187d3e03e79a2f7f966bc36e
Kilian <kilian.volb@protonmail.com>
2017-08-24 05:00:16 +0200

    cleanup

--- view/src/app-server.js
+++ view/src/app-server.js
@@ -1,7 +1,32 @@
-import {
-  createApp
-} from './app.js'
+import { createApp } from './app.js'
 
+/**
+ * Helper function for recursive asyncData calling of matched components
+ */
+const callAsyncRecursive = (parent, params) => {
+  // First object isn't passed as component array/collection
+  if (parent.components) {
+    callAsyncRecursive(parent.components, params)
+    return parent.asyncData ? parent.asyncData(params) : 0
+  }
+  // Sub components are traversed
+  else {
+    for (let property in parent) {
+      let obj = parent[property]
+      // More sub components? recursively traverse them too
+      if (obj.components) {
+        callAsyncRecursive(obj.components, params)
+      }
+      if (obj.asyncData) {
+        return obj.asyncData(params)
+      }
+    }
+  }
+}
+
+/**
+ * Exports
+ */
 export default context => {
   return new Promise((resolve, reject) => {
     const {
@@ -29,35 +54,14 @@ export default context => {
           store,
           route: router.currentRoute
         }
-        const callAsyncRecursive = parent => {
-          // First component isn't passed with component array
-          if (parent.components) {
-            callAsyncRecursive(parent.components)
-            return parent.asyncData ? parent.asyncData(params) : 0
-          }
-          // Sub components are traversed
-          else {
-            for (let property in parent) {
-              let obj = parent[property]
-              // More sub components? recursively traverse them too
-              if (obj.components) {
-                callAsyncRecursive(obj.components)
-              }
-              if (obj.asyncData) {
-                return obj.asyncData(params)
-              }
-            }
-          }
-        }
-        return callAsyncRecursive(loaded)
+        return callAsyncRecursive(loaded, params)
       })).then(() => {
-        // After all preFetch hooks are resolved, our store is now
+        // After all asyncData hooks are resolved, our store is now
         // filled with the state needed to render the app.
         // Expose the state on the render context, and let the request handler
         // inline the state in the HTML response. This allows the client-side
         // store to pick-up the server-side state without having to duplicate
         // the initial data fetching on the client.
-        console.log(store.state)
         context.state = store.state
         resolve(app)
       })
