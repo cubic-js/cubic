f6fccbbeeccf05c4b529c3773d72ff8fe445cf28
Kilian <kilian.volb@protonmail.com>
2017-08-25 20:43:46 +0200

    override

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-const extend = require('deep-extend')
-const Connection = require('./lib/connection.js')
+const Connection = require("./lib/connection.js")
 
 class Blitz {
+
   /**
    * Merge default options with client options
    */
-  constructor (options) {
+  constructor(options) {
     this.connecting = null
-    this.options = extend({
+    this.options = Object.assign({
 
       // Resource Config
-      api_url: 'http://localhost:3010/',
-      auth_url: 'http://localhost:3030/',
+      api_url: "http://localhost:3010/",
+      auth_url: "http://localhost:3030/",
 
       // Connection Config
-      use_socket: true,
       namespace: '/',
 
       // Authorization Config
@@ -26,87 +25,93 @@ class Blitz {
     // Remove "/" from url's
     let api = this.options.api_url
     let auth = this.options.auth_url
-    this.options.api_url = api[api.length - 1] === '/' ? api.slice(0, -1) : api
-    this.options.auth_url = auth[auth.length - 1] === '/' ? auth.slice(0, -1) : auth
+    this.options.api_url = api[api.length - 1] === "/" ? api.slice(0, -1) : api
+    this.options.auth_url = auth[auth.length - 1] === "/" ? auth.slice(0, -1) : auth
+
     this.connect()
   }
+
 
   /**
    * Connect by getting tokens and setting up clients
    */
-  async connect () {
+  async connect() {
     this.connection = new Connection(this.options)
     this.connecting = this.connection.connect()
     await this.connecting
     this.connecting = null
   }
 
+
   /**
    * Subscribe to certain endpoints
    */
-  async subscribe (endpoint) {
+  async subscribe(endpoint) {
     await this.connecting
-    this.emit('subscribe', endpoint)
+    this.emit("subscribe", endpoint)
   }
+
 
   /**
    * Event listening for socket.io
    */
-  async on (ev, func) {
+  async on(ev, func) {
     await this.connecting
     this.connection.client.on(ev, func)
   }
 
+
   /**
    * Expose Socket client emit
    */
-  async emit (ev, data) {
+  async emit(ev, data) {
     await this.connecting
     this.connection.client.emit(ev, data)
   }
 
+
   /**
    * RESTful methods for manual interaction
    */
-  async query (verb, query) {
+  async query(verb, query) {
     await this.connecting
     return this.connection.request(verb, query)
   }
 
-  get (query) {
-    return this.query('GET', query)
+  get(query) {
+    return this.query("GET", query)
   }
 
-  post (url, body) {
+  post(url, body) {
     let query = {
       url: url,
       body: body
     }
-    return this.query('POST', query)
+    return this.query("POST", query)
   }
 
-  put (url, body) {
+  put(url, body) {
     let query = {
       url: url,
       body: body
     }
-    return this.query('PUT', query)
+    return this.query("PUT", query)
   }
 
-  patch (url, body) {
+  patch(url, body) {
     let query = {
       url: url,
       body: body
     }
-    return this.query('PATCH', query)
+    return this.query("PATCH", query)
   }
 
-  delete (url, body) {
+  delete(url, body) {
     let query = {
       url: url,
       body: body
     }
-    return this.query('DELETE', query)
+    return this.query("DELETE", query)
   }
 }
 
