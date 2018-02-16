83900ada619ae83f2f6fcd0445e914c214563452
Kaptard <kilian@nexus-stats.com>
2018-02-16 09:52:11 +0100

    refactor: integrate socket parser into adapter

--- connections/adapters/sockets.js
+++ connections/adapters/sockets.js
@@ -1,6 +1,6 @@
 const Adapter = require('./adapter.js')
 const io = require('socket.io')
-const converter = require('../../middleware/socketParser.js')
+const middleware = require('../../middleware/native/sockets.js')
 const Layer = require('../layers.js')
 
 /**
@@ -33,6 +33,95 @@ class SocketAdapter extends Adapter {
       await layer.runStack(req, res, this.stack)
       this.pass(req, res)
     } catch(err) {}
+  }
+
+  /**
+   * Convert Socket.io request into req-like object
+   */
+  convertReq (request, socket, verb) {
+    if (request) {
+      let req = {}
+      let url = verb === 'GET' ? request : request.url
+
+      req.body = request.body
+      req.url = url
+      req.user = socket.user
+      req.method = verb
+      req.channel = 'Sockets'
+      req.query = {}
+      req.params = {}
+
+      return req
+    } else {
+      return {}
+    }
+  }
+
+  /**
+   * Convert Socket.io ack callback into res-like object
+   */
+  convertRes (socket, ack) {
+    // Default response value
+    let res = {
+      statusCode: 200,
+      body: ''
+    }
+
+    // Socket.io ack passed?
+    if (ack) {
+      // Send method, invoking client callback with previously customized data
+      res.send = (data) => {
+        if (!res.sent) {
+          res.sent = true
+          res.body = data
+          ack(res)
+        } else {
+          // Multi request. No errors will occur but this shouldn't happen
+        }
+      }
+
+      // Apply Status before res.send
+      res.status = (code) => {
+        res.statusCode = code
+        return res
+      }
+
+      // Pseudo res.json to stay parallel with express
+      res.json = (data) => {
+        data = JSON.stringify(data)
+        return res.send(data)
+      }
+
+      res.redirect = (status, data) => {
+        status = typeof status === 'number' ? status : 302
+        data = typeof status === 'number' ? data : status
+        return res.status(status).send(data)
+      }
+    }
+
+    // Non-ack request
+    else {
+      // Simple socket emit
+      res.send = (data) => {
+        res.body = data
+        socket.emit('res', res.msg)
+      }
+
+      // Apply Status before res.send
+      res.status = (code) => {
+        res.statusCode = code
+        return res
+      }
+
+      // Pseudo res.json to stay parallel with express
+      res.json = (data) => {
+        data = JSON.stringify(data)
+        return res.send(data)
+      }
+    }
+
+    // Modified res object
+    return res
   }
 }
 
