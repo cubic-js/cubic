46d3189b69b5de413e77a9d875ce18b0b105c7c1
Kaptard <kilian@nexus-stats.com>
2018-02-16 18:30:46 +0100

    simplify node entry point

--- index.js
+++ index.js
@@ -1,70 +1,18 @@
 const local = require('./config/local.js')
-const ipc = require('blitz-js-loader/lib/worker')
 const Server = require('./connections/server.js')
 
-/**
- * Parent Class for API-Node
- */
 class API {
-  /**
-   * Set config for blitz.js to merge
-   * @constructor
-   */
   constructor (options) {
-    // Process forked
-    if (process.env.isWorker) {
-      this.setup = ipc.setGlobal()
-      this.setup.then(() => this.init())
-      ipc.expose(this)
-    }
-
-    // Process not forked
-    else {
-      // Config which is called by blitz.js on blitz.use()
-      this.config = {
-        local: local,
-        provided: options
-      }
-
-      // Path for forking
-      this.filename = __filename
+    this.config = {
+      local: local,
+      provided: options
     }
   }
 
-  init () {
-    this.server = new Server()
-  }
-
-  /**
-   * Run any function from a remote process with `this` context
-   */
-  run (fn) {
-    return fn.apply(this)
-  }
-
-  use (route, fn) {
-    this.server.use(route, fn)
-  }
-
-  get (route, fn) {
-    this.server.use(route, fn, 'GET')
-  }
-
-  post (route, fn) {
-    this.server.use(route, fn, 'POST')
-  }
-
-  put (route, fn) {
-    this.server.use(route, fn, 'PUT')
-  }
-
-  patch (route, fn) {
-    this.server.use(route, fn, 'PATCH')
-  }
-
-  delete (route, fn) {
-    this.server.use(route, fn, 'DELETE')
+  init() {
+    const id = this.config.provided.id || this.config.local.id
+    this.server = new Server(id)
   }
 }
 
-module.exports = process.env.isWorker ? new API() : API
+module.exports = API
