b61226f2fd0537f7609a2361dabee2790a394aea
Kaptard <kilian@nexus-stats.com>
2018-02-19 00:29:11 +0100

    refactor: all-in-one loader no longer takes custom config. Use individual nodes instead.

--- index.js
+++ index.js
@@ -1,36 +1,29 @@
-const extend = require('deep-extend')
-const local = require('./config/local.js')
+const BlitzLoader = require('../blitz-js-loader')
+const intro = require('./intro.js')
+const Auth = require('../blitz-js-auth')
+const Api = require('../blitz-js-api')
+const Core = require('../blitz-js-core')
+const View = require('../blitz-js-view')
 
-module.exports = function(config) {
-  config = extend(local, config)
-  const Blitz = require('blitz-js-loader')(config.blitz)
-  const intro = require('./logger.js')
-  const Auth = require('blitz-js-auth')
-  const Api = require('blitz-js-api')
-  const Core = require('blitz-js-core')
-  const View = require('blitz-js-view')
+class Blitz {
+  /**
+   * Load blitz-js with default nodes
+   */
+  load (config) {
+    this.init(config)
+    blitz.use(new Api())
+    blitz.use(new Core())
+    blitz.use(new Auth())
+    blitz.use(new View())
+  }
 
   /**
-   * Authentication node user management and token generation
+   * Load blitz-js without default nodes
    */
-  config.auth.hooks.forEach(hook => blitz.hook('auth_core', hook))
-  blitz.use(new Auth(config.auth))
-
-  /**
-   * API node to distribute incoming requests to core nodes
-   */
-  config.api.hooks.forEach(hook => blitz.hook(config.api.id || Api, hook))
-  blitz.use(new Api(config.api))
-
-  /**
-   * Core node which handles the actual processing of requests
-   */
-  config.core.hooks.forEach(hook => blitz.hook(config.core.id || Core, hook))
-  blitz.use(new Core(config.core))
-
-  /**
-   * View node which renders visual webpages
-   */
-  config.view.hooks.forEach(hook => blitz.hook('view_core', hook))
-  blitz.use(new View(config.view))
+  init (config) {
+    BlitzLoader(config)
+    intro.roll()
+  }
 }
+
+module.exports = new Blitz
---
+++ intro.js
@@ -0,0 +1,60 @@
+/**
+ * Dependencies
+ */
+const chalk = require("chalk")
+const version = require('./package.json').version
+const pkg = require(`${process.cwd()}/package.json`)
+
+/**
+ * Main Logger class. Used for interface fluff & Sentry.io
+ */
+class Intro {
+
+  constructor() {
+    this.border = chalk.grey("---------------------------------------------------------------")
+  }
+
+  /**
+   * Big Useless intro
+   */
+  roll() {
+    console.log('\x1B[2J\x1B[0f\u001b[0;0H')
+    console.log(" ")
+    console.log(chalk.grey(`:: ${pkg.name} v.${pkg.version}`))
+    console.log(chalk.grey(":: Launching Blitz.js stack..."))
+    console.log(" ")
+    console.log(chalk.grey("---------------------------------------------------------------"))
+    console.log(" ")
+    console.log(`
+                            ..
+                            :Ndo-
+                             .dMMms:
+                               sMMMMNh:.
+                                /NMMMMMMdo.
+                                 .mMMMMMMMMd.
+                 /:-..            \`yMMMMMMMM/
+                \`MMMMMMNmdys+-      /MMMMMMM/
+                \`MMNNMMMMMms:        -mMMMMM/
+                \`MMMh  .:\`            \`yMMMM/
+                \`MMMMm.            \`:. .sMMM/
+                \`MMMMMN.        -odMMMMMMNMM/
+                \`MMMMMMMs      .:oyhdNMMMMMM/
+                \`MMMMMMMMd.            \`.-::.
+                 :MMMMMMMMN:
+                  \`:hNMMMMMMs
+                     \`:sNMMMMd.      Blitz.js
+                         -odMMN:     ${chalk.green(`v.${version}`)}
+                            \`+hNo
+                                \`
+                         `)
+    console.log(" ")
+    console.log(" ")
+    console.log(chalk.grey(":: " + new Date()))
+    console.log(":: Environment : " + blitz.config.local.environment)
+    console.log(":: LogLevel    : " + blitz.config.local.logLevel)
+    console.log(chalk.grey("---------------------------------------------------------------"))
+    console.log(" ")
+  }
+}
+
+module.exports = new Intro
