ff313b2086544ffb39b514bc6678d1bdf434cd18
Kaptard <kilian@nexus-stats.com>
2018-02-23 12:26:11 +0100

    change default api port

--- config/local.js
+++ config/local.js
@@ -3,7 +3,7 @@ module.exports = {
   /**
    * Current Node Information
    */
-  port: 3010,
+  port: 3001,
   routes: __dirname + '/../connections/entry/routes.js',
   events: __dirname + '/../connections/entry/events.js',
 
