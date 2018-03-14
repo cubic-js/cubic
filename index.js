48c5ad521302f552b09ab42484bfeaaba6bf2d6f
kilian@nexus-stats.com <kilian@nexus-stats.com>
2018-03-14 16:04:11 +0100

    fix: Have bootstrap function work before blitz is loaded

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const BlitzLoader = require('blitz-js-loader')
+const load = require('blitz-js-loader')
 const Auth = require('blitz-js-auth')
 const Api = require('blitz-js-api')
 const Core = require('blitz-js-core')
@@ -6,20 +6,46 @@ const View = require('blitz-js-view')
 const intro = require('./intro.js')
 
 class Blitz {
-  constructor (config) {
-    const Blitz = BlitzLoader(config)
-    intro.roll()
-    return Blitz
+  constructor(options) {
+    this.options = options
+  }
+
+  /**
+   * Ensure blitz-js-loader is loaded
+   */
+  init() {
+    try { blitz }
+    catch (err) {
+      load(this.options)
+      intro.roll()
+    }
   }
 
   /**
    * Load blitz-js with default nodes
    */
   bootstrap () {
+    this.init()
     blitz.use(new Api())
     blitz.use(new Core())
     blitz.use(new Auth())
     blitz.use(new View())
+  }
+
+  /**
+   * Imitate blitz.hook to hook functions before a node is loaded
+   */
+  hook(node, fn) {
+    this.init()
+    blitz.hook(node, fn)
+  }
+
+  /**
+   * Imitate blitz.use to load new nodes
+   */
+  use(node) {
+    this.init()
+    blitz.use(node)
   }
 }
 
