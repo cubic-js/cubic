81e427d9a579a297fe60c2ee398163d4c079ec25
kilian@nexus-stats.com <kilian@nexus-stats.com>
2018-03-23 16:29:07 +0100

    fix: Prevent auth check when no scope is required

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,7 @@ class Auth {
    * Check user scope before processing request
    */
   verify(req, res, endpoint) {
-    if (!req.user.scp.includes(endpoint.scope) && !req.user.scp.includes('write_root')) {
+    if (endpoint.scope && !req.user.scp.includes(endpoint.scope) && !req.user.scp.includes('write_root')) {
       return res.status(403).send({
         error: 'Unauthorized',
         reason: `Expected ${endpoint.scope}, got ${req.user.scp}.`
