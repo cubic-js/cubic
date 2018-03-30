18d18fd8730227eded9d2c0b3c32a65988e8fef4
kilian@nexus-stats.com <kilian@nexus-stats.com>
2018-03-30 20:49:39 +0200

    feat: Add userkey API endpoint to retrieve a user's user_key

---
+++ endpoints/userkey.js
@@ -0,0 +1,47 @@
+const Endpoint = require(blitz.config.auth.core.endpointParent)
+const auth = require('../lib/auth.js')
+const crypto = require('crypto')
+const randtoken = require('rand-token').generator({ source: crypto.randomBytes })
+const bcrypt = require('bcryptjs')
+
+/**
+ * Get the User Key for a given user. Important when authenticating through the
+ * API after the user_key from /register has been discarded
+ */
+class UserKey extends Endpoint {
+  constructor(api, db, url) {
+    super(api, db, url)
+    this.schema.method = 'POST'
+  }
+
+  async main(req, res) {
+    const user_id = req.body.user_id
+    const user_secret = req.body.user_secret
+    this.getUserKey(user_id, user_secret, res)
+  }
+
+  /**
+   * Find the user key (Assumes the user is verified). The user_key isn't really
+   * that sensitive, but not public either, so we'd rather keep it safe.
+   */
+  async getUserKey(user_id, user_secret, res) {
+    const user = await this.db.collection('users').findOne({ user_id })
+
+    try {
+      await bcrypt.compare(user_secret, user.user_secret)
+    } catch (err) {
+      auth.saveIP.bind(this)(credentials.user_key, ip, 'credentials', false)
+      res.status(403).send({
+        error: 'Unauhtorized.',
+        reason: 'Credentials not recognized.'
+      })
+      return
+    }
+
+    // Verified
+    auth.saveIP.bind(this)(user.user_key, ip, 'getUserKey', true)
+    res.send({ user_key: user.user_key })
+  }
+}
+
+module.exports = UserKey
