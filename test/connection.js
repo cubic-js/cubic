85469cb3f5c24d1f19f86204546ba13d8a455881
kilian@nexus-stats.com <kilian@nexus-stats.com>
2018-04-07 03:10:18 +0200

    ci: Add drone-ci setup

--- test/connection.js
+++ test/connection.js
@@ -4,17 +4,30 @@ const Api = require('cubic-api')
 const Auth = require('cubic-auth')
 const Core = require(process.cwd())
 
+// Config
+const endpointPath = `${process.cwd()}/test/endpoints`
+const publicPath = `${process.cwd()}/test/assets`
+const redisUrl = 'redis://redis'
+const mongoUrl = 'mongodb://mongodb'
+const ci = process.env.DRONE_CI
+
+
 /**
  * Load up cubic api to connect to and auth node to authenticate at.
  */
 before(async () => {
   loader({ logLevel: 'silent' })
-  await cubic.use(new Auth())
-  await cubic.use(new Api())
-  await cubic.use(new Core({
-    endpointPath: `${process.cwd()}/test/endpoints`,
-    publicPath:`${process.cwd()}/test/assets`
-  }))
+  await cubic.use(new Auth(ci ? {
+    api: { redisUrl },
+    core: { redisUrl, mongoUrl }
+  } : {}))
+  await cubic.use(new Api(ci ? { redisUrl } : {}))
+  await cubic.use(new Core(ci ? {
+    endpointPath,
+    publicPath,
+    redisUrl,
+    mongoUrl
+  } : { endpointPath, publicPath }))
 })
 
 
