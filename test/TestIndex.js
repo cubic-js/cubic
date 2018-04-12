784f9877c3458e036e09896977ff48896420e658
kilian@nexus-stats.com <kilian@nexus-stats.com>
2018-04-12 11:06:48 +0200

    ci: remove condition for `after` flag

--- test/TestIndex.js
+++ test/TestIndex.js
@@ -46,11 +46,9 @@ describe('/index.js', function() {
   })
 
   // Remove default files
-  if (!ci) {
-    after(async function() {
-      await removeFile(`${process.cwd()}/api`)
-      await removeFile(`${process.cwd()}/assets`)
-      await removeFile(`${process.cwd()}/ui`)
-    })
-  }
+  after(async function() {
+    await removeFile(`${process.cwd()}/api`)
+    await removeFile(`${process.cwd()}/assets`)
+    await removeFile(`${process.cwd()}/ui`)
+  })
 })
