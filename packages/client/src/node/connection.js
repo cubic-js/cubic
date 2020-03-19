650f746fdb181dde5e93547cc11241904cbccc1d
Nakroma <tarackobarack@gmail.com>
2020-03-19 19:07:51 +0100

    Added basic on message handling with heartbeat

--- packages/client/src/node/connection.js
+++ packages/client/src/node/connection.js
@@ -11,6 +11,7 @@ class Connection {
     this.options = options
     // this.request = { delay: this.options.requestDelay || 500, counter: 0 }
     // this.reconnect = { delay: this.options.reconnectDelay || 500, counter: 0 }
+    this.lastHeartbeat = new Date()
     this.mutex = new Mutex()
   }
 
@@ -26,12 +27,27 @@ class Connection {
   async _createConnection () {
     const wss = new WebSocket(this.url)
     wss.onopen = () => console.log('connection open')
-    wss.onerror = (error) => console.log(`WebSocket Error: ${error.message}`) // https://developer.mozilla.org/en-US/docs/Web/API/ErrorEvent
+    wss.onerror = (error) => console.log(`WebSocket Error: ${error.message}`)
     // TODO: Implement reconnect https://github.com/websockets/ws/blob/HEAD/doc/ws.md#event-close-1
     // https://developer.mozilla.org/en-US/docs/Web/API/CloseEvent
     wss.onclose = (close) => console.log(`connection closed with code ${close.code}`)
-    wss.onmessage = (message) => console.log(`connection received message: ${message.data}`) // https://developer.mozilla.org/de/docs/Web/API/MessageEvent
+    wss.onmessage = (message) => this._onMessage(message.data)
     this.connection = wss
+  }
+
+  /**
+   * WebSocket message handling
+   */
+  async _onMessage (data) {
+    data = JSON.parse(data)
+    console.log(`connection received message: ${data}`)
+
+    // Heartbeat
+    if (typeof data === 'string' && data.startsWith('primus::ping::')) {
+      this.lastHeartbeat = new Date()
+      this.connection.send(JSON.stringify(data.replace('ping', 'pong')))
+      console.log(`sending ${JSON.stringify(data.replace('ping', 'pong'))}`)
+    }
   }
 }
 
