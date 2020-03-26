1617e17d2b9e9bac7202861c7b261e9be4daec60
Nakroma <tarackobarack@gmail.com>
2020-03-26 16:53:14 +0100

    feat(client): Completely reworked request and retry handling

--- packages/client/src/browser/connection.js
+++ packages/client/src/browser/connection.js
@@ -6,6 +6,7 @@ class Connection extends NodeConnection {
     const url = this.apiAccessToken ? `${this.url}?bearer=${this.apiAccessToken}` : this.url
 
     const wss = new WS(url)
+    wss.onopen = () => this._resumeConnection()
     wss.onerror = (error) => console.log(`WebSocket Error: ${error.message}`)
     wss.onclose = (close) => { if (close.code !== 1000) this._reconnect() } // Not closed deliberately
     wss.onmessage = (message) => this._onMessage(message.data)
